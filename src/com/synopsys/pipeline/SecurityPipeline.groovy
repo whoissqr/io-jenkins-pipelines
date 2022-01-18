847bd06af3a5cb8a890950b1e2b8d3daaa779713
Neil Pathare <pathare@synopsys.com>
2022-01-18 15:55:00 -0500

    Adding Java - Shared Library

---
+++ src/com/synopsys/pipeline/SecurityPipeline.groovy
@@ -0,0 +1,101 @@
+#! /bin/groovy
+
+package com.synopsys.pipeline
+
+/**
+ * This script is the entry point for the shared library pipeline.
+ * It defines pipeline stages and manages overall control flow in the pipeline.
+ */
+def execute() {
+    node('master') {
+
+        stage('Checkout Code') {
+            git branch: 'devsecops', url: 'https://github.com/devsecops-test/vulnado'
+        }
+
+        stage('Build Source Code') {
+            sh '''mvn clean compile'''
+        }
+
+        stage('IO - Setup Prescription') {
+            echo 'Setup Prescription'
+            synopsysIO(connectors: [
+                io(
+                    configName: 'io-azure',
+                    projectName: 'devsecops-vulnado',
+                    workflowVersion: '2021.12.2'),
+                github(
+                    branch: 'devsecops',
+                    configName: 'github-devsecops',
+                    owner: 'devsecops-test',
+                    repositoryName: 'vulnado'),
+                buildBreaker(configName: 'BB-ALL')]) {
+                    sh 'io --stage io'
+                }
+        }
+
+        stage('IO - Read Prescription') {
+            def prescriptionJSON = readJSON file: 'io_state.json'
+            print("Updated Prescription JSON :\n$prescriptionJSON\n")
+            print("SAST Enabled: $prescriptionJSON.Data.Prescription.Security.Activities.Sast.Enabled")
+            print("SCA Enabled: $prescriptionJSON.Data.Prescription.Security.Activities.Sca.Enabled")
+            print("BusinessCriticalityScore: $prescriptionJSON.Data.Prescription.RiskScore.BusinessCriticalityScore")
+            print("DataClassScore: $prescriptionJSON.Data.Prescription.RiskScore.DataClassScore")
+            print("AccessScore: $prescriptionJSON.Data.Prescription.RiskScore.AccessScore")
+            print("ToolingScore: $prescriptionJSON.Data.Prescription.RiskScore.ToolingScore")
+            print("TrainingScore: $prescriptionJSON.Data.Prescription.RiskScore.TrainingScore")
+        }
+
+        stage('SAST- RapidScan') {
+            environment {
+                OSTYPE = 'linux-gnu'
+            }
+            steps {
+                echo 'Running SAST using Sigma - Rapid Scan'
+                echo env.OSTYPE
+                synopsysIO(connectors: [
+                    rapidScan(configName: 'Sigma')]) {
+                    sh 'io --stage execution --state io_state.json'
+                }
+            }
+        }
+
+        stage('SAST - Polaris') {
+            echo 'Running SAST using Polaris'
+            synopsysIO(connectors: [
+                [$class: 'PolarisPipelineConfig',
+                configName: 'csprod-polaris',
+                projectName: 'sig-devsecops/vulnado']]) {
+                sh 'io --stage execution --state io_state.json'
+            }
+        }
+
+        stage('SCA - Black Duck') {
+            echo 'Running SCA using Black Duck'
+            synopsysIO(connectors: [
+                blackduck(configName: 'BIZDevBD',
+                projectName: 'vulnado',
+                projectVersion: '1.0')]) {
+                sh 'io --stage execution --state io_state.json'
+            }
+        }
+
+        stage('IO - Workflow') {
+            echo 'Execute Workflow Stage'
+            synopsysIO() {
+                synopsysIO() {
+                    sh 'io --stage workflow --state io_state.json'
+                }
+            }
+        }
+
+        stage('IO - Archive') {
+            // Archive Results & Logs
+            archiveArtifacts artifacts: '**/*-results*.json', allowEmptyArchive: 'true'
+            archiveArtifacts artifacts: '.io/**', allowEmptyArchive: 'true'
+
+            // Remove the state json file as it has sensitive information
+            sh 'rm io_state.json'
+        }
+    }
+}
