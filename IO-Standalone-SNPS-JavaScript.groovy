634c1f687ecc10b200ebffa4048b8abf6da30f68
Neil Pathare <pathare@synopsys.com>
2022-01-18 15:16:44 -0500

    Adding SNS JavaScript pipeline

---
+++ IO-Standalone-SNPS-JavaScript.groovy
@@ -0,0 +1,94 @@
+pipeline {
+    agent any
+    tools {
+        nodejs 'NodeJS'
+    }
+    stages {
+        stage('Checkout Source Code') {
+            steps {
+                git branch: 'devsecops', url: 'https://github.com/devsecops-test/vulnerable-node'
+            }
+        }
+
+        stage('Build Source Code') {
+            steps {
+                sh 'npm install > npm-install.log'
+            }
+        }
+
+        stage('IO - Prescription') {
+            steps {
+                synopsysIO(connectors: [
+                    io(
+                        configName: 'io-azure',
+                        projectName: 'devsecops-vulnerable-node',
+                        workflowVersion: '2021.12.2'),
+                    github(
+                        branch: 'devsecops',
+                        configName: 'github-devsecops',
+                        owner: 'devsecops-test',
+                        repositoryName: 'vulnerable-node'),
+                    buildBreaker(configName: 'BB-ALL')]) {
+                        sh 'io --stage io'
+                    }
+            }
+        }
+
+        stage('SAST - RapidScan (Sigma)') {
+            environment {
+                OSTYPE = 'linux-gnu'
+            }
+            steps {
+                echo 'Running SAST using Sigma - Rapid Scan'
+                echo env.OSTYPE
+                synopsysIO(connectors: [
+                    rapidScan(configName: 'Sigma')]) {
+                    sh 'io --stage execution --state io_state.json'
+                }
+            }
+        }
+
+        stage('SAST - Polaris') {
+            steps {
+                echo 'Running SAST using Polaris'
+                synopsysIO(connectors: [
+                    [$class: 'PolarisPipelineConfig',
+                    configName: 'csprod-polaris',
+                    projectName: 'devsecops-test/vulnerable-node']]) {
+                    sh 'io --stage execution --state io_state.json'
+                }
+            }
+        }
+
+        stage('SCA - BlackDuck') {
+            steps {
+              echo 'Running SCA using BlackDuck'
+              synopsysIO(connectors: [
+                  blackduck(configName: 'BIZDevBD',
+                  projectName: 'vulnerable-node',
+                  projectVersion: '1.0')]) {
+                  sh 'io --stage execution --state io_state.json'
+              }
+            }
+        }
+
+        stage('IO - Workflow') {
+            steps {
+                echo 'Execute Workflow Stage'
+                synopsysIO() {
+                    sh 'io --stage workflow --state io_state.json'
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Archive Results & Logs
+            archiveArtifacts artifacts: '.io/**', allowEmptyArchive: 'true'
+
+            // Remove the state json file as it has sensitive information
+            sh 'rm io_state.json'
+        }
+    }
+}
