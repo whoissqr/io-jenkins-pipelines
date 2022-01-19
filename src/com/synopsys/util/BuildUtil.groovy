39d12488a32719eef2543a367378c6276054f836
Neil Pathare <pathare@synopsys.com>
2022-01-19 14:06:24 -0500

    Updating doc

--- src/com/synopsys/util/BuildUtil.groovy
+++ src/com/synopsys/util/BuildUtil.groovy
@@ -3,6 +3,7 @@ package com.synopsys.util
 /**
  * A utility class to store build execution methods.
  * This replaces the "tools { maven: 'Maven3' }"" section of a standard pipeline script.
+ * https://www.jenkins.io/doc/book/pipeline/shared-libraries/#accessing-steps
  */
 class BuildUtil implements Serializable {
 
