5be00de25b111a8a0c75de9e71e0162cb2bbb326
Neil Pathare <pathare@synopsys.com>
2022-01-21 09:17:50 -0500

    Test update

--- vars/SASTPlusM.groovy
+++ vars/SASTPlusM.groovy
@@ -1,18 +1,17 @@
 def call() {
     pipeline {
         stage('SAST Plus Manual') {
-            when {
-                expression { isSASTPlusMEnabled }
-            }
-            input {
-                message 'Major code change detected, manual code review (SAST - Manual) triggerd. Proceed?'
-                ok 'Approve'
-                parameters {
-                    string(name: 'ApprovalComment', defaultValue: 'Approved', description: 'Approval Comment.')
+            if (isSASTPlusMEnabled) {
+                input {
+                    message 'Major code change detected, manual code review (SAST - Manual) triggerd. Proceed?'
+                    ok 'Approve'
+                    parameters {
+                        string(name: 'ApprovalComment', defaultValue: 'Approved', description: 'Approval Comment.')
+                    }
                 }
-            }
-            steps {
-                echo "Out-of-Band Activity - SAST Plus Manual triggered & approved with comment: {$ApprovalComment}."
+                steps {
+                    echo "Out-of-Band Activity - SAST Plus Manual triggered & approved with comment: {$ApprovalComment}."
+                }
             }
         }
     }
