7e7b9df2c98b581ceae19ccd33b0f1553fd457d8
Neil Pathare <pathare@synopsys.com>
2022-01-21 09:51:59 -0500

    Test update

--- src/com/synopsys/pipeline/SecurityPipeline.groovy
+++ src/com/synopsys/pipeline/SecurityPipeline.groovy
@@ -90,24 +90,16 @@ def execute() {
             }
         }
 
-        if (isSASTPlusMEnabled) {
-            SASTPlusM(isSASTPlusMEnabled)
+        stage('SAST Plus Manual') {
+            if (isSASTPlusMEnabled) {
+                def userInput = input (
+                    message: 'Major code change detected, manual code review (SAST - Manual) triggerd. Proceed?',
+                    ok: 'Approve',
+                    ) {
+                                echo "Out-of-Band Activity - SAST Plus Manual triggered & approved with comment: {$ApprovalComment}."
+                            }
+            }
         }
-
-        // stage('SAST Plus Manual') {
-        //     if (isSASTPlusMEnabled) {
-        //         def userInput = input (
-        //             message: "Major code change detected, manual code review (SAST - Manual) triggerd. Proceed?",
-        //             ok: "Approve",
-        //             parameters: [
-        //                 string: [
-        //                     name: 'ApprovalComment',
-        //                     defaultValue: 'Approved',
-        //                     description: 'Approval Comment.']]) {
-        //                         echo "Out-of-Band Activity - SAST Plus Manual triggered & approved with comment: {$ApprovalComment}."
-        //                     }
-        //     }
-        // }
 
         stage('SCA - BlackDuck') {
             if (isSCAEnabled) {
