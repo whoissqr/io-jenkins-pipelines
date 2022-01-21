0c9f4d04e76c783530f90e74071e01a080149d86
Neil Pathare <pathare@synopsys.com>
2022-01-21 10:01:36 -0500

    Test update

--- src/com/synopsys/pipeline/SecurityPipeline.groovy
+++ src/com/synopsys/pipeline/SecurityPipeline.groovy
@@ -98,7 +98,8 @@ def execute() {
                 //     ) {
                 //         echo "Out-of-Band Activity - SAST Plus Manual triggered & approved with comment: {$ApprovalComment}."
                 //     }
-                def userInput = input message: 'Major code change detected, manual code review (SAST - Manual) triggerd. Proceed?', ok: 'Go ahead'
+                def userInput = input message: 'Major code change detected, manual code review (SAST - Manual) triggerd. Proceed?', ok: 'Go ahead', parameters: string(name: 'Comment', defaultValue: 'Approved', description: 'Approval Comment.')
+                }
 
                 echo "${userInput}"
             }
