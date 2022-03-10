094c52b3c7762ef7aa671c88cdae7e38b83f0a1a
Neil Pathare <pathare@synopsys.com>
2022-03-10 12:01:29 -0500

    Added Execution, Workflow & Signoff stages

--- IO-Standalone-SNPS-Java-ExternalSCM.groovy
+++ IO-Standalone-SNPS-Java-ExternalSCM.groovy
@@ -1,10 +1,15 @@
+def isSASTEnabled
+def isSASTPlusMEnabled
+def isSCAEnabled
+def isDASTEnabled
+def isDASTPlusMEnabled
+def buildBreakerStatus
+
 pipeline {
     agent any
-
     tools {
         maven 'Maven3'
     }
-
     environment {
         SYNOPSYS_IO_Scm_CodePatch = sh(script: 'git diff HEAD^ HEAD', , returnStdout: true)
     }
@@ -61,6 +66,99 @@ pipeline {
                     print("ImageScan Enabled: $isImageScanEnabled")
                 }
             }
+        }
+
+        stage('SAST - RapidScan (Sigma)') {
+            when {
+                expression { isSASTEnabled }
+            }
+            environment {
+                OSTYPE = 'linux-gnu'
+            }
+            steps {
+                echo 'Running SAST using Sigma - Rapid Scan'
+                synopsysIO(connectors: [
+                    rapidScan(configName: 'Sigma')]) {
+                    sh 'io --stage execution --state io_state.json'
+                }
+            }
+        }
+
+        stage('SAST - Polaris') {
+            when {
+                expression { isSASTEnabled }
+            }
+            steps {
+                echo 'Running SAST using Polaris'
+                synopsysIO(connectors: [
+                    [$class: 'PolarisPipelineConfig',
+                    configName: 'csprod-polaris',
+                    projectName: 'sig-devsecops/spring-boot-demo']]) {
+                    sh 'io --stage execution --state io_state.json'
+                }
+            }
+        }
+
+        stage('SCA - BlackDuck') {
+            when {
+                expression { isSCAEnabled }
+            }
+            steps {
+              echo 'Running SCA using BlackDuck'
+              synopsysIO(connectors: [
+                  blackduck(configName: 'BIZDevBD',
+                  projectName: 'spring-boot-demo',
+                  projectVersion: '0.0.1-SNAPSHOT')]) {
+                  sh 'io --stage execution --state io_state.json'
+              }
+            }
+        }
+
+        stage('IO - Workflow') {
+            steps {
+                echo 'Execute Workflow Stage'
+                synopsysIO(connectors: [
+                    codeDx(configName: 'SIG-CodeDx', projectId: '6'),
+                    buildBreaker(configName: 'BB-Custom')
+                ]) {
+                    sh 'io --stage workflow --state io_state.json'
+                }
+
+                script {
+                    def workflowJSON = readJSON file: 'wf-output.json'
+                    print("========================== IO WorkflowEngine Summary ============================")
+                    print("Breaker Status: $workflowJSON.breaker.status")
+
+                    buildBreakerStatus = workflowJSON.breaker.status
+                    workflowJSON.breaker.criteria.each{activity->
+                        print("Activity: ${activity.activityname}")
+                        if(activity.has("overall")) {
+                            overallCount = activity.overall.size()
+                            print("Build Breaker Issue Count: $overallCount")
+                        } else if(activity.has("risk_score")) {
+                            print("CodeDx Risk Score: ${activity.risk_score}")
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Security Sign-Off') {
+            steps {
+                script {
+                    if (buildBreakerStatus) {
+                        input message: 'One or more conditions triggered Build Breaker. Do you wish to proceed?'
+                    }
+                }
+                echo "Security Sign-Off triggered & approved"
+            }
+        }
+    }
+
+    post {
+        always {
+            // Always remove the state JSON file as it has sensitive information
+            sh 'rm io_state.json'
         }
     }
 }
